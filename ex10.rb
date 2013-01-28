b636a3fec57312f5b11fc1a8fdcaa1f539b06972
Matthew Gould <matt@curebit.com>
2013-01-27 20:47:05 -0500

    now has a sound with \a character

--- ex10.rb
+++ ex10.rb
@@ -1,4 +1,5 @@
-tabby_cat = "\tI'm tabbed in."
+#cool, the backslash a -- \a makes a sound!!  its' calle d a bell alert (bell labs)
+tabby_cat = "\tI'm tabbed in.\a"
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
