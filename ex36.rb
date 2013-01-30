f1bb16d3dae646199ceb766183acd5e447874e9d
Matthew Gould <matt@curebit.com>
2013-01-30 17:07:45 -0500

    didnt' do this one b/c it would take a long time adn seemed boring.  Might be cool to do some sort of 'Walk Through' with Curebit though.  Split it up into before register, post register, campaigns, integration, etc.

---
+++ ex36.rb
@@ -0,0 +1,26 @@
+#Curebit Walk Through
+
+
+def prompt()
+	p "> "
+end
+
+puts "I decided against doing this b/c it woudl take too long.  but i get the point."
+
+# have_registered?
+# y/n
+
+# small_or_medium_or_enterprise
+
+# create_campaign?
+# y/n
+
+# want_register?
+# y/n
+
+# go_register
+# end
+
+
+
+
