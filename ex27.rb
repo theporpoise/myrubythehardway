d196cd2e754be1b97bacbd26767ad141ced674e1
Matthew Gould <matt@curebit.com>
2013-01-30 07:28:49 -0500

    no code here, just truth tables

---
+++ ex27.rb
@@ -0,0 +1,57 @@
+# #Review of the truth tables
+
+# The Truth Terms:
+
+# and 
+# or
+# not
+# !=
+# ==
+# >=
+# <=
+# true
+# false
+
+# The Truth Tables:
+
+# NOT 				True?
+# not false			true
+# not true 			false
+
+# OR 					True?
+# true or false 		true
+# false or true 		true
+# true or true 		true
+# false or false		false
+
+# And 				True?
+# true and false		false
+# false and true 		false
+# true and true 		true
+# false and false		false
+
+# NOT OR 				True?
+# not(true or false)	false
+# not(false or true)	false
+# not(true or true) 	false
+# not(false or false)	true
+
+# NOT AND 				True?
+# not(true and false)		true
+# not(false and true)		true
+# not(true and true)		false
+# not(false and false)	true
+
+# !=		True?
+# 1 != 0	true
+# 1 != 1 	false
+# 0 != 1	true
+# 0 != 0	false
+
+# ==		True?
+# 1 == 0 	false
+# 1 == 1 	true
+# 0 == 1	false
+# 0 == 0	true
+
+
