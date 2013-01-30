e2500ad5bf29a4329edc9023b21e9f8cc0369dd3
Matthew Gould <matt@curebit.com>
2013-01-30 17:19:07 -0500

    updated ex36 with reasoning why I didn't do it.. have to rationalize to myself... lol

--- ex36.rb
+++ ex36.rb
@@ -1,5 +1,11 @@
 #Curebit Walk Through
 
+#Ok, I had a thought on this file for why I shouldn't do it - I have too much else to do!
+
+# This is a great project for someone who doesn't have a project.  But I have plenty of 
+# projects to do.  B/c I have plenty of projects to do, motivation is not the problem.
+# 	I do not need a project to motivate me. Therefore, I'm just focused on learning.
+#If I want a project to do, I'll do it for real, for work... lol...
 
 def prompt()
 	p "> "
